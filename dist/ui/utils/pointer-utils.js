36a37c3c31c1d4d20aa3c0aec595393821c10b90
sentoku870 <rongtaifutian@gmail.com>
2025-11-20 22:21:49 +0900

    Remove dist from tracking and add to .gitignore

--- dist/ui/utils/pointer-utils.js
+++
@@ -1,9 +0,0 @@
-const TOUCH_POINTER_TYPES = new Set(['touch', 'pen']);
-export function isPointerActive(event) {
-    var _a;
-    if (typeof event.buttons === 'number' && event.buttons > 0) {
-        return true;
-    }
-    return TOUCH_POINTER_TYPES.has((_a = event.pointerType) !== null && _a !== void 0 ? _a : '');
-}
-//# sourceMappingURL=pointer-utils.js.map
